7f6d69e6b03d0a577f4e5b459a3e37b53dd39bc6
Black Slash Creative <ross@blacksla.sh>
2023-05-31 11:10:44 -0700

    removes old fetch functionality

--- src/Components/products.jsx
+++ src/Components/products.jsx
@@ -27,71 +27,7 @@ const products = [
 //=========Cart=============
 const Cart = (props) => {
   let list = [];
-  let data = props.location.data ? props.location.data : products;
-  // console.log(`data:${JSON.stringify(data)}`);
-
   return <Accordion defaultActiveKey="0">{list}</Accordion>;
-};
-
-const useDataApi = (initialUrl, initialData) => {
-  const { useState, useEffect, useReducer } = React;
-  const [url, setUrl] = useState(initialUrl);
-
-  const [state, dispatch] = useReducer(dataFetchReducer, {
-    isLoading: false,
-    isError: false,
-    data: initialData,
-  });
-  // console.log(`useDataApi called`);
-  useEffect(() => {
-    console.log("useEffect Called");
-    let didCancel = false;
-    const fetchData = async () => {
-      dispatch({ type: "FETCH_INIT" });
-      try {
-        const result = await axios.post(url);
-        // console.log("FETCH FROM URL");
-        if (!didCancel) {
-          dispatch({ type: "FETCH_SUCCESS", payload: result.data });
-        }
-      } catch (error) {
-        if (!didCancel) {
-          dispatch({ type: "FETCH_FAILURE" });
-        }
-      }
-    };
-    fetchData();
-    return () => {
-      didCancel = true;
-    };
-  }, [url]);
-  return [state, setUrl];
-};
-const dataFetchReducer = (state, action) => {
-  switch (action.type) {
-    case "FETCH_INIT":
-      return {
-        ...state,
-        isLoading: true,
-        isError: false,
-      };
-    case "FETCH_SUCCESS":
-      console.log('FETCH_SUCCESS');
-      return {
-        ...state,
-        isLoading: false,
-        isError: false,
-        data: action.payload,
-      };
-    case "FETCH_FAILURE":
-      return {
-        ...state,
-        isLoading: false,
-        isError: true,
-      };
-    default:
-      throw new Error();
-  }
 };
 
 const Products = (props) => {
